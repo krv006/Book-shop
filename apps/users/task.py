ac395e1a19e774953be0af8799d0fa6bb8d2de85
krv006 <rvkamronbek@gmail.com>
2024-10-16 19:29:23 +0500

    celery change settings

--- apps/users/task.py
+++ apps/users/task.py
@@ -23,7 +23,7 @@ def send_activation_email_task(subject, message, recipient_list, html_message):
         delete_cache_task.apply_async((cache_key,), countdown=60)
     return f"Emails sent to {', '.join(recipient_list)} and cache keys created."
 
-# @shared_task
+# @
 # def delete_cache_task(cache_key):
 #     cache.delete(cache_key)
 #     return f"Cache key {cache_key} successfully deleted."
