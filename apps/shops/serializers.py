7df76eb7dd81d54dcfc1231e4f89cb15b8136917
krv006 <rvkamronbek@gmail.com>
2024-10-17 21:22:58 +0500

    add address

--- apps/shops/serializers.py
+++ apps/shops/serializers.py
@@ -1,20 +1,50 @@
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from rest_framework.exceptions import ValidationError
+from rest_framework.fields import HiddenField, CurrentUserDefault, CharField, EmailField, IntegerField, BooleanField
+from rest_framework.serializers import Serializer, ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from shops.models import Address, Country
+from users.models import User
 
 
 class CountryModelSerializer(ModelSerializer):
     class Meta:
         model = Country
-        fields = '__all__'
+        exclude = ()
 
 
-class AddressModelSerializer(ModelSerializer):
+class AddressListModelSerializer(ModelSerializer):
+    user = HiddenField(default=CurrentUserDefault())
+    postal_code = IntegerField(default=123400, min_value=0)
+    has_shipping_address = BooleanField(write_only=True)
+    has_billing_address = BooleanField(write_only=True)
+
     class Meta:
         model = Address
-        fields = '__all__'
+        exclude = ()
 
-    def to_representation(self, instance: Address):
+    def create(self, validated_data):
+        _has_billing_address = validated_data.pop('has_billing_address')
+        _has_shipping_address = validated_data.pop('has_shipping_address')
+
+        _address = super().create(validated_data)
+        _user: User = _address.user
+        if _has_billing_address:
+            _user.billing_address = _address
+            _user.save()
+
+        if _has_shipping_address:
+            _user.shipping_address = _address
+            _user.save()
+
+        return _address
+
+    def to_representation(self, instance):
         repr = super().to_representation(instance)
         repr['country'] = CountryModelSerializer(instance.country).data
+        repr['has_billing_address'] = instance.user.billing_address_id == instance.id
+        repr['has_shipping_address'] = instance.user.shipping_address_id == instance.id
         return repr
--- apps/shops/urls.py
+++ apps/shops/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from shops.views import AddressListCreateAPIView, CountryListAPIView
+from shops.views import AddressListCreateAPIView, CountryListAPIView, AddressDestroyUpdateAPIView
 
 router = DefaultRouter()
 # router.register(r'wishlists', WishListViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+
+    path('address', AddressListCreateAPIView.as_view(), name='address_list'),
+    path('address/<int:pk>', AddressDestroyUpdateAPIView.as_view(), name='address_detail'),
+
     path('address', AddressListCreateAPIView.as_view()),
     path('country', CountryListAPIView.as_view()),
 ]
